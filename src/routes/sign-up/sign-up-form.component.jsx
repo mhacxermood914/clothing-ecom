a07db95e2c0c2f25384cb8ac1eebe72f5d824aa3
mhacxermood <mhacxermood@gmail.com>
2022-08-16 11:25:48 +0100

    feat: Button Component

--- src/routes/sign-up/sign-up-form.component.jsx
+++ src/routes/sign-up/sign-up-form.component.jsx
@@ -1,6 +1,8 @@
 import React, { useState, useRef } from 'react'
 import FormInput from '../../components/form-input/form-input.component'
+import Button from '../../components/button/button.component'
 import { createAuthUserWithMailAndPassword, createUserDocumentFromAuth } from './../../utils/firebase/firebase'
+import './sign-up-form.styles.scss'
 const defaultFormFields = {
     displayName: '',
     email: '',
@@ -48,14 +50,15 @@ const SignUpForm = () => {
 
 
   return (
-    <div>
-        <h1>Sign up with your email</h1>
+    <div className='sign-up-container'>
+        <h2>Don't have an account?</h2>
+        <span>Sign up with your email and password</span>
         <form onSubmit={handleSubmit} ref={form}>
             <FormInput label="Display Name" type="text" name="displayName" required  onChange={handleChange} value={displayName}/>
             <FormInput label="Email" type="email" name="email" required onChange={handleChange} value={email}  />
             <FormInput label="Password" type="password" name="password" required onChange={handleChange} value={password} />
             <FormInput label="Confirm Password" type="password" name="confirmPassword" required  onChange={handleChange} value={confirmPassword}/>
-            <button type="submit">Submit</button>
+            <Button type="submit" buttonType='inverted'>Submit</Button>
         </form>
     </div>
   )
