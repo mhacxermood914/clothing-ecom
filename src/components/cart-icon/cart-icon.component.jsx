e71a2a44080e081d91ee35f0fea6c99185b5674b
mhacxermood <mhacxermood@gmail.com>
2022-08-21 12:02:24 +0100

    feat: Cart Item Count;

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,20 +1,20 @@
-import React, { useContext } from 'react';
+import { useContext } from 'react';
+
+import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
+
 import { CartContext } from '../../contexts/cart.context';
-import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg'
+
 import './cart-icon.styles.scss';
 
 const CartIcon = () => {
+  const { isCartOpen, setIsCartOpen, cartCount } = useContext(CartContext);
 
-const {isCartOpen, setIsCartOpen }= useContext(CartContext)
-
-  const toogle = ()=> {
-    setIsCartOpen(!isCartOpen)
-  }
+  const toggleIsCartOpen = () => setIsCartOpen(!isCartOpen);
 
   return (
-    <div className='cart-icon-container' onClick={toogle}>
+    <div className='cart-icon-container' onClick={toggleIsCartOpen}>
       <ShoppingIcon className='shopping-icon' />
-      <span className='item-count'>0</span>
+      <span className='item-count'>{cartCount}</span>
     </div>
   );
 };
